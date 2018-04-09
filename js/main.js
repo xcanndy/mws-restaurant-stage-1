1dcc7bfbd8520f47835a352e2c8b4271670d712b
xcanndy <ola.chmur@gmail.com>
2018-04-09 18:51:24 +0200

    semantic change (h1 -> h3)

--- js/main.js
+++ js/main.js
@@ -148,7 +148,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = `${restaurant.name} restaurant`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
