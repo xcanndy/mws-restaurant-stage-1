8aad4be5d8d991b0a209ab9f3eed1a36389fa78a
xcanndy <ola.chmur@gmail.com>
2018-04-16 22:27:57 +0200

    jpg extension added to photograph

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -147,7 +147,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
