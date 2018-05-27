7e1ec4caffd28e43ef67770f9e30689df9540e8e
xcanndy <ola.chmur@gmail.com>
2018-05-27 20:26:23 +0200

    Static Maps working

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -9,13 +9,13 @@ window.initMap = () => {
     if (error) { // Got an error!
       console.error(error);
     } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: restaurant.latlng,
-        scrollwheel: false
-      });
+      // self.map = new google.maps.Map(document.getElementById('map'), {
+      //   zoom: 16,
+      //   center: restaurant.latlng,
+      //   scrollwheel: false
+      // });
       fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+      // DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
     }
   });
 }
