ae560a7407408e51b392f981a1a852c726bb7e3f
xcanndy <ola.chmur@gmail.com>
2018-06-09 14:49:02 +0200

    remove initMap

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -4,21 +4,32 @@ var map;
 /**
  * Initialize Google map, called from HTML.
  */
-window.initMap = () => {
+// window.initMap = () => {
+//   fetchRestaurantFromURL((error, restaurant) => {
+//     if (error) { // Got an error!
+//       console.error(error);
+//     } else {
+//       // self.map = new google.maps.Map(document.getElementById('map'), {
+//       //   zoom: 16,
+//       //   center: restaurant.latlng,
+//       //   scrollwheel: false
+//       // });
+//       fillBreadcrumb();
+//       // DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+//     }
+//   });
+// }
+
+window.onload = () => {
   fetchRestaurantFromURL((error, restaurant) => {
-    if (error) { // Got an error!
-      console.error(error);
+    if (error) {
+      console.log(error);
     } else {
-      // self.map = new google.maps.Map(document.getElementById('map'), {
-      //   zoom: 16,
-      //   center: restaurant.latlng,
-      //   scrollwheel: false
-      // });
       fillBreadcrumb();
-      // DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
     }
   });
 }
+
 
 /**
  * Get current restaurant from page URL.
